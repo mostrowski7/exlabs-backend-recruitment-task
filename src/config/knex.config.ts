dcc6d52b0031491705cac99f197601e3128d900a
mostrowski7 <ursusenszaman@gmail.com>
2023-06-18 15:25:14 +0200

    feat: add knex migration and seed for user table

---
+++ src/config/knex.config.ts
@@ -0,0 +1,25 @@
+import { Knex } from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../../.env' });
+
+const knexConfig: Knex.Config = {
+  client: 'pg',
+  connection: {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5432,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  },
+  migrations: {
+    extension: 'ts',
+    directory: '../infra/database/migrations',
+  },
+  seeds: {
+    directory: '../infra/database/seeds',
+    extension: 'ts',
+  },
+};
+
+export default knexConfig;
