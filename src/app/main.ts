fe7836c044afb6a4db40cb8c613513d71b924d64
mostrowski7 <ursusenszaman@gmail.com>
2023-07-01 17:34:57 +0200

    refactor: add wrapper for express server

---
+++ src/app/main.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import config from '../config';
+import logger from '../utils/logger';
+import databaseConnection from '../infra/database/database-connection';
+import createServer from './server';
+
+async function main() {
+  const app = createServer();
+
+  await databaseConnection();
+
+  app.listen(config.port, async () => {
+    logger.info(`Server listening on port ${config.port}`);
+  });
+}
+
+main();
--- src/app/server.ts
+++ src/app/server.ts
@@ -1,29 +1,23 @@
-import 'reflect-metadata';
-import express from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import config from '../config';
-import logger from '../utils/logger';
 import errorHandler from '../interfaces/middleware/error-handler.middleware';
-import databaseConnection from '../infra/database/database-connection';
 import routes from '../interfaces/routes';
 
-const app = express();
+function createServer(): Application {
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-app.use(cors());
+  app.use(cors());
 
-app.use(helmet());
+  app.use(helmet());
 
-app.use('/api', routes);
+  app.use('/api', routes);
 
-app.use(errorHandler);
+  app.use(errorHandler);
 
-app.listen(config.port, async () => {
-  logger.info(`Server listening on port ${config.port}`);
+  return app;
+}
 
-  await databaseConnection();
-});
-
-export default app;
+export default createServer;
